a3e67c790f0f2cf516b7ef4d9e225f958d86c195
Emre Yilmaz <emre.yilmaz@metglobal.com>
2017-12-26 10:23:41 +0300

    change wording

--- hidden_hook.py
+++ hidden_hook.py
@@ -67,7 +67,7 @@ def post_to_discord(hook_url, message):
 def check_posts(connection_uri, webhook_url):
     posts = get_last_hidden_posts()
     for post in posts:
-        message = "**[%s]** - %s flagged contribution: %s" % (
+        message = "**[%s]** - %s hidden contribution: %s" % (
             post.get("json_metadata", {}).get("type", "Unknown"),
             post["moderator"],
             "https://utopian.io" + post["url"]
